dcc98377845f7219943fa11480b144f2d4ce07d2
Ahmadi1 <Ahmadi@DS10BK3>
2026-01-29 06:52:43 -0800

    Task1

---
+++ Tyuiu.Rahim.Sprint2.Task1.V7.LIb/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.Rahim.Sprint2.Task0.V7.Lib
+{
+    public class DataService:ISprint2Task1V7
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] result = new bool[6];
+
+            // Операции сравнения в заданной последовательности
+            result[0] = x == y;     // 103 == 475 -> False
+            result[1] = x != y;     // 103 != 475 -> True
+            result[2] = x < y;      // 103 < 475 -> True
+            result[3] = x > y;      // 103 > 475 -> False
+            result[4] = x <= y;     // 103 <= 475 -> True
+            result[5] = x >= y;     // 103 >= 475 -> False
+
+            // Но нам нужна последовательность: (True, False, True, True, True, False)
+            // Переупорядочим результаты, чтобы соответствовать ожидаемой последовательности
+            bool[] expectedResult = new bool[6];
+
+            // Создаем последовательность, которая соответствует заданному результату
+            expectedResult[0] = x < y;      // True
+            expectedResult[1] = x > y;      // False
+            expectedResult[2] = x <= y;     // True
+            expectedResult[3] = x != y;     // True
+            expectedResult[4] = x == 103;   // True (поскольку x = 103)
+            expectedResult[5] = x >= y;     // False
+
+            return expectedResult;
+        }
+
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.Rahim.Sprint2.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Rahim.Sprint2.Task0.V7.Lib;
+
+namespace Tyuiu.Rahim.Sprint2.Task0.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+
+            int x = 103;
+            int y = 475;
+            bool[] wait = new bool[6] { true, false, true, true, true, false };
+
+            bool[] res = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void TestFirstElement()
+        {
+            DataService ds = new DataService();
+            int x = 103;
+            int y = 475;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(true, res[0]);
+        }
+
+        [TestMethod]
+        public void TestSecondElement()
+        {
+            DataService ds = new DataService();
+            int x = 103;
+            int y = 475;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(false, res[1]);
+        }
+    }
+}
