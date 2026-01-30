2624e308558bc657bda6a281fd170e36a71723fc
Ahmadi1 <Ahmadi@DS10BK3>
2026-01-30 04:06:34 -0800

    Task3

--- Tyuiu.Rahim.Sprint2.Task3.V21.Lib/DataService.cs
+++ Tyuiu.Rahim.Sprint2.Task3.V21.Lib/DataService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
 
 namespace Tyuiu.Rahim.Sprint2.Task3.V21.Lib
 {
-    public class DataService
+    public class DataService:ISprint2Task3V21
     {
         public double Calculate(double x)
         {
@@ -10,14 +11,12 @@ namespace Tyuiu.Rahim.Sprint2.Task3.V21.Lib
 
             if (x > 1)
             {
-                // y = x * ((x+9)/(x-1))^x
-                double fraction = (x + 9) / (x - 1);
-                y = x * Math.Pow(fraction, x);
+                y = Math.Pow(x + 9, x) / (x - 1);
             }
             else if (x == 0)
             {
                 y = (Math.Pow(x, 2) + 10) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
-                // برای x=0: (0+10)/(0 - sin(0) + 12) = 10/12 = 0.833333...
+                // وقتی x=0 باشد: (0+10)/(0 - sin(0) + 12) = 10/12 = 0.833333...
             }
             else if (x > -25 && x < 2)  // -25 < x < 2
             {
@@ -25,12 +24,15 @@ namespace Tyuiu.Rahim.Sprint2.Task3.V21.Lib
             }
             else if (x < -25)
             {
-                y = 11 * x - (1 / x);  // x + 10x - 1/x = 11x - 1/x
+                y = x + 10 * x - (1 / x);  // x + 10x - (1/x) = 11x - 1/x
             }
             else
             {
-                // مقادیر مرزی: x=1, x=2, x=-25
-                throw new ArgumentException($"Значение x={x} не попадает ни в один диапазон вычислений");
+                // برای x=1 یا x=2 یا x=-25 (مقادیر مرزی)
+                // طبق شرط: x>1 جداگانه، x=0 جداگانه، -25<x<2 جداگانه، x<-25 جداگانه
+                // پس x=1 و x=2 و x=-25 در هیچ‌کدام نیستند؟ باید بررسی شود
+                // طبق منطق: x=1 در هیچ‌کدام نیست، پس خطا می‌دهیم یا مقدار پیش‌فرض
+                throw new ArgumentException("Значение x не попадает ни в один диапазон вычислений");
             }
 
             return Math.Round(y, 3);
